995355945d3e684fab9d2ff402da87002e41ea35
Florian Hines <florian.hines@gmail.com>
2011-07-01 01:11:08 -0500

    Expand swift-recon.py cmd utility to include most swift-recon metrics. Mostly working but needs lots of cleanup.

--- bin/swift-recon.py
+++ bin/swift-recon.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 """
-    Perfrom cluster reconnaissance
+    cmdline utility to perfrom cluster reconnaissance
 """
 
 
@@ -14,6 +14,7 @@ import optparse
 import os
 
 VERBOSE = False
+SUPPRESS_ERRORS = False
 
 
 def getdevices():
@@ -24,7 +25,7 @@ def getdevices():
 
 
 def scout(base_url, recon_type):
-    global VERBOSE
+    global VERBOSE, SUPPRESS_ERRORS
     url = base_url + recon_type
     try:
         body = urllib2.urlopen(url).read()
@@ -33,32 +34,58 @@ def scout(base_url, recon_type):
             print "-> %s: %s" % (url, content)
         status = 200
     except urllib2.HTTPError as e:
-        print "-> %s: %s" % (url, e)
+        if not SUPPRESS_ERRORS or VERBOSE:
+            print "-> %s: %s" % (url, e)
         content = e
         status = e.code
     except urllib2.URLError as e:
-        print "-> %s: %s" % (url, e)
+        if not SUPPRESS_ERRORS or VERBOSE:
+            print "-> %s: %s" % (url, e)
         content = e
         status = -1
     return url, content, status
 
 
 def scout_md5(host):
-    global VERBOSE
     base_url = "http://%s:%s/recon/" % (host[0], host[1])
     url, content, status = scout(base_url, "ringmd5")
     return url, content, status
 
 
 def scout_async(host):
-    global VERBOSE, ASYNC_COUNTER
     base_url = "http://%s:%s/recon/" % (host[0], host[1])
     url, content, status = scout(base_url, "async")
     return url, content, status
 
 
+def scout_replication(host):
+    base_url = "http://%s:%s/recon/" % (host[0], host[1])
+    url, content, status = scout(base_url, "replication")
+    return url, content, status
+
+
+def scout_load(host):
+    base_url = "http://%s:%s/recon/" % (host[0], host[1])
+    url, content, status = scout(base_url, "load")
+    return url, content, status
+
+
+def scout_du(host):
+    base_url = "http://%s:%s/recon/" % (host[0], host[1])
+    url, content, status = scout(base_url, "diskusage")
+    return url, content, status
+
+
+def scout_umount(host):
+    base_url = "http://%s:%s/recon/" % (host[0], host[1])
+    url, content, status = scout(base_url, "unmounted")
+    return url, content, status
+
+
 def get_ringmd5(ringfile):
     stats = {}
+    matches = 0
+    errors = 0
     hosts = getdevices()
     md5sum = md5()
     with open(ringfile, 'rb') as f:
@@ -77,11 +104,17 @@ def get_ringmd5(ringfile):
             #fixme - need to grab from config
             stats[url] = response[ringfile]
             if response[ringfile] != ring_sum:
-                print "!! %s (%s) doesn't match on disk md5sum" % (url,
-                    response[ringfile])
+                ringsmatch = False
+                print "!! %s (%s) doesn't match on disk md5sum" % \
+                    (url, response[ringfile])
             else:
+                matches = matches + 1
                 if VERBOSE:
                     print "-> %s matches." % url
+        else:
+            errors = errors + 1
+    print "%s/%s hosts matched, %s error[s] while checking hosts." % \
+            (matches, len(hosts), errors)
     print "=" * 79
 
 
@@ -102,20 +135,125 @@ def async_check():
         average = total / len(stats)
         print "Async stats: low: %d, high: %d, avg: %d, total: %d" % (low,
             high, average, total)
-        print "=" * 79
     else:
         print "Error: No hosts where available or returned valid information."
+    print "=" * 79
+
+
+def umount_check():
+    ASYNC_COUNTER = 0
+    stats = {}
+    hosts = getdevices()
+    pool = eventlet.GreenPool(20)
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print "[%s] Getting unmounted drives from %s hosts..." % (now, len(hosts))
+    for url, response, status in pool.imap(scout_umount, hosts):
+        if status == 200:
+            for i in response:
+                stats[url] = i['device']
+    for host in stats:
+        print "Not mounted: %s on %s" % (stats[host], host)
+    print "=" * 79
+
+
+def replication_check():
+    stats = {}
+    hosts = getdevices()
+    pool = eventlet.GreenPool(20)
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print "[%s] Checking replication times on %s hosts..." % (now, len(hosts))
+    for url, response, status in pool.imap(scout_replication, hosts):
+        if status == 200:
+            stats[url] = response['object_replication_time']
+    if len(stats) > 0:
+        low = min(stats.values())
+        high = max(stats.values())
+        total = sum(stats.values())
+        average = total / len(stats)
+        print "[Replication Times] shortest: %s, longest: %s, avg: %s" % \
+            (low, high, average)
+    else:
+        print "Error: No hosts where available or returned valid information."
+    print "=" * 79
+
+
+def load_check():
+    load1 = {}
+    load5 = {}
+    load15 = {}
+    hosts = getdevices()
+    pool = eventlet.GreenPool(20)
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print "[%s] Checking load avg's on %s hosts..." % (now, len(hosts))
+    for url, response, status in pool.imap(scout_load, hosts):
+        if status == 200:
+            load1[url] = response['1m']
+            load5[url] = response['5m']
+            load15[url] = response['15m']
+    stats = {"1m": load1, "5m": load5, "15m": load15}
+    for item in stats:
+        if len(stats[item]) > 0:
+            low = min(stats[item].values())
+            high = max(stats[item].values())
+            total = sum(stats[item].values())
+            average = total / len(stats[item])
+            print "[%s load average] lowest: %s, highest: %s, avg: %s" % \
+                (item, low, high, average)
+        else:
+            print "Error: Hosts unavailable or returned valid information."
+    print "=" * 79
+
+
+def disk_usage():
+    hosts = getdevices()
+    stats = {}
+    highs = []
+    lows = []
+    averages = []
+    pool = eventlet.GreenPool(20)
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print "[%s] Checking disk usage on %s hosts..." % (now, len(hosts))
+    for url, response, status in pool.imap(scout_du, hosts):
+        if status == 200:
+            hostusage = []
+            for entry in response:
+                if entry['mounted']:
+                    used = float(entry['used']) / float(entry['size']) * 100.0
+                    hostusage.append(round(used, 2))
+            stats[url] = hostusage
+    for url in stats:
+        if len(stats[url]) > 0:
+            low = min(stats[url])
+            high = max(stats[url])
+            total = sum(stats[url])
+            average = total / len(stats[url])
+            highs.append(high)
+            lows.append(low)
+            averages.append(average)
+        else:
+            print "-> %s: Error. No drive info available." % url
+    if len(lows) > 0:
+        low = min(lows)
+        high = max(highs)
+        average = sum(averages) / len(averages)
+        print "Disk usage: lowest: %s%%, highest: %s%%, avg: %s%%" % \
+            (low, high, average)
+    else:
+        print "Error: No hosts where available or returned valid information."
+    print "=" * 79
 
 
 def main():
-    global VERBOSE, swift_dir, pool
+    global VERBOSE, SUPPRESS_ERRORS, swift_dir, pool
     print "=" * 79
     usage = '''
-    usage: %prog [-v] [-a] [-r] [-u] [-d] [-l] [-c] [--objmd5]
+    usage: %prog [-v] [--suppress] [-a] [-r] [-u] [-d] [-l] [-c] [--objmd5]
     '''
     args = optparse.OptionParser(usage)
     args.add_option('--verbose', '-v', action="store_true",
         help="Print verbose info")
+    args.add_option('--suppress', action="store_true",
+        help="Suppress most connection related errors")
     args.add_option('--async', '-a', action="store_true",
         help="Get async stats")
     args.add_option('--replication', '-r', action="store_true",
@@ -136,12 +274,19 @@ def main():
 
     swift_dir = options.swiftdir
 
-    if options.verbose is True:
-        VERBOSE = True
-    if options.async is True:
+    VERBOSE = options.verbose
+    SUPPRESS_ERRORS = options.suppress
+
+    if options.async:
         async_check()
-    if options.replication is True:
+    if options.unmounted:
+        umount_check()
+    if options.replication:
         replication_check()
+    if options.loadstats:
+        load_check()
+    if options.diskusage:
+        disk_usage()
     if options.objmd5:
         get_ringmd5(os.path.join(swift_dir, 'container.ring.gz'))
 
